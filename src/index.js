f0dbe82240d0cd6d21358cea36642b1f78b42b5a
betterLitao <62136669+betterLitao@users.noreply.github.com>
2025-08-08 09:41:49 +0800

    Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log('Hello, GitHub Tools Demo!');
+
+// This is a simple demo file to showcase file pushing to GitHub
+function demoFunction() {
+  return 'This is a demo function';
+}
+
+module.exports = demoFunction;
