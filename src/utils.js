2d6169ee4967d0c11e7014f0431954355f2706f7
betterLitao <62136669+betterLitao@users.noreply.github.com>
2025-08-08 14:09:51 +0800

    Add memoize utility function
    - Added a memoize function to cache results of expensive function calls
    - Memoize helps improve performance by avoiding repeated calculations
    - Exported the new memoize function
    
    Ref: #6

--- src/utils.js
+++ src/utils.js
@@ -51,10 +51,25 @@ function debounce(func, delay) {
   };
 }
 
+// Memoize function to cache results of expensive function calls
+function memoize(func) {
+  const cache = new Map();
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+    const result = func.apply(this, args);
+    cache.set(key, result);
+    return result;
+  };
+}
+
 module.exports = {
   formatDate,
   capitalize,
   generateId,
   deepClone,
-  debounce
+  debounce,
+  memoize
 };
