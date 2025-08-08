4643afc7d0935fc4989c111f8d9147d820596e9e
betterLitao <62136669+betterLitao@users.noreply.github.com>
2025-08-08 11:31:08 +0800

    Add new utility functions to utils.js
    Added generateId, deepClone, and debounce utility functions to enhance the utility library.

--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,54 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+// New utility function to generate a random ID
+function generateId(length = 8) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+// New utility function to deep clone an object
+function deepClone(obj) {
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
+  }
+  
+  if (obj instanceof Date) {
+    return new Date(obj.getTime());
+  }
+  
+  if (obj instanceof Array) {
+    return obj.map(item => deepClone(item));
+  }
+  
+  if (obj instanceof Object) {
+    const clonedObj = {};
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        clonedObj[key] = deepClone(obj[key]);
+      }
+    }
+    return clonedObj;
+  }
+}
+
+// New utility function to debounce a function
+function debounce(func, delay) {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(this, args), delay);
+  };
+}
+
 module.exports = {
   formatDate,
-  capitalize
+  capitalize,
+  generateId,
+  deepClone,
+  debounce
 };
