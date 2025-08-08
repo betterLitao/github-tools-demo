c608d9782fed82f10876815a6ec67d621a3ae974
betterLitao <62136669+betterLitao@users.noreply.github.com>
2025-08-08 09:41:56 +0800

    Add src/utils.js with utility functions

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+// Utility functions for the demo
+function formatDate(date) {
+  return date.toISOString().split('T')[0];
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+module.exports = {
+  formatDate,
+  capitalize
+};
